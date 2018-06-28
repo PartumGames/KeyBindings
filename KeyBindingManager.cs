7ca5f77c77f28c0deb0da0f8a201733897223ba9
PartumGames <37095216+PartumGames@users.noreply.github.com>
2018-06-28 17:14:57 -0500

    Add files via upload

---
+++ KeyBindingManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindingManager : MonoBehaviour {
+
+    private List<string> keys;
+    public List<Dropdown> dropdowns = new List<Dropdown>();
+
+    public KeyCode jumpKey;
+    public KeyCode pauseKey;
+    public KeyCode inventoryKey;
+
+    void Start()
+    {
+        keys = new List<string> { "Space", "A", "B", "C", "D", "I", "S", "M" };
+        for (int i = 0; i < dropdowns.Count; i++)
+        {
+            dropdowns[i].AddOptions(keys);
+        }
+
+        PlayerPrefs.SetString("jumpPrefs", "M");
+        LoadPrefabs();
+    }
+
+  
+    private void LoadPrefabs()
+    {
+        jumpKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpPrefs"));
+        Select_Key(dropdowns[0], jumpKey.ToString());
+
+        pauseKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("pausePrefs"));
+        Select_Key(dropdowns[1], pauseKey.ToString());
+
+        inventoryKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("inventoryPrefs"));
+        Select_Key(dropdowns[2], inventoryKey.ToString());
+
+    }
+
+    private void Select_Key(Dropdown _dropdown, string _s)
+    {
+        for (int i = 0; i < keys.Count; i++)
+        {
+            if(_s == keys[i])
+            {
+                _dropdown.value = i;
+            }
+        }
+    }
+
+    public void ChangeJumpKey(int id)
+    {
+        jumpKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), keys[id]);
+        PlayerPrefs.SetString("jumpPrefs", keys[id]);
+    }
+
+    public void ChangePauseKey(int id)
+    {
+        pauseKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), keys[id]);
+        PlayerPrefs.SetString("pausePrefs", keys[id]);
+    }
+
+    public void ChangeInventoryKey(int id)
+    {
+        inventoryKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), keys[id]);
+        PlayerPrefs.SetString("inventoryPrefs", keys[id]);
+    }
+
+}
---
+++ UseKeys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseKeys : MonoBehaviour {
+
+    public KeyBindingManager keyBindings;
+
+
+    void Update(){
+        if (Input.GetKeyDown(keyBindings.jumpKey))
+        {
+            print("the jump key is: " + keyBindings.jumpKey.ToString());
+        }
+
+        if (Input.GetKeyDown(keyBindings.pauseKey))
+        {
+            print("the pause key is: " + keyBindings.pauseKey.ToString());
+        }
+
+        if (Input.GetKeyDown(keyBindings.inventoryKey))
+        {
+            print("the inventory key is: " + keyBindings.inventoryKey.ToString());
+        }
+    }
+
+
+//end o class
+}
